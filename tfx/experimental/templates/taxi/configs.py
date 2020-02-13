78073690ab24a85d3e4eae594617e674b94b5496
jiyongjung <jiyongjung@google.com>
2020-02-13 00:55:57 -0800

    Deletes an incompatible and unused field in taxi template.
    PiperOrigin-RevId: 294855977

--- tfx/experimental/templates/taxi/configs.py
+++ tfx/experimental/templates/taxi/configs.py
@@ -75,7 +75,6 @@ _query_sample_rate = 0.0001  # Generate a 0.01% random sample.
 #           dropoff_latitude,
 #           dropoff_longitude,
 #           trip_miles,
-#           pickup_census_tract,
 #           dropoff_census_tract,
 #           payment_type,
 #           company,
